fc669a263e3a2a85927a7bf154bde3effe4d6c44
Frances Cao <frances.te.cao@gmail.com>
2021-12-11 08:41:33 -0500

    Add code for matching

--- src/containers/Matches/Matches.js
+++ src/containers/Matches/Matches.js
@@ -4,28 +4,41 @@ import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Grid from 'react-bootstrap/lib/Grid';
 import '../../shared/styles.css';
 import {SCHEDULER_URL} from "../../constants";
+import { withRouter } from 'react-router-dom';
 
 
 class Matches extends Component {
     constructor(props) {
         super(props);
+        this.routeChange = this.routeChange.bind(this);
+        this.getMatch = this.getMatch.bind(this);
         this.state = {}
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+        const userId = this.props.match.params.userId;
+        this.props.onGetUser(userId);
+    }
 
     // Get match by GET /api/matchUser/<uid>
-    getMatch = (id) => {
-        if (id !== null) {
-            fetch(SCHEDULER_URL + "/matchUser/" + id, {method: 'GET'});
+    getMatch = (userId) => {
+        if (userId !== null) {
+            fetch(SCHEDULER_URL + "/matchUser/" + userId, {method: 'GET'});
         }
     }
+
+    routeChange() {
+        let matchid = this.getMatch(this.props.match.params.userId);
+        let path = `users/` + matchid;
+        this.props.history.push(path);
+  }
 
     render() {
         return(
             <Grid>
                 <ButtonGroup className="d-flex-default">
-                    <Button primary bsStyle="danger" bsSize="large" onClick={() => this.getMatch("5")}>Get Match</Button>
+                    {/*<Button primary bsStyle="danger" bsSize="large" onClick={() => this.getMatch("5")}>Get Match</Button>*/}
+                    <Button primary bsStyle="danger" bsSize="large" onClick={this.routeChange}>Get Match</Button>
                 </ButtonGroup>
             </Grid>
         );
