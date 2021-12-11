08c592e4e749adf0490e07b154c14f73b9a5f209
howieraem <34226262+howieraem@users.noreply.github.com>
2021-12-11 11:25:27 -0500

    Update Matches.js

--- src/containers/Matches/Matches.js
+++ src/containers/Matches/Matches.js
@@ -1,28 +1,21 @@
-import React, { Component, setState } from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Grid from 'react-bootstrap/lib/Grid';
 import '../../shared/styles.css';
 import {SCHEDULER_URL} from "../../constants";
-import { withRouter } from 'react-router-dom';
 
 
 class Matches extends Component {
     constructor(props) {
         super(props);
-        this.routeChange = this.routeChange.bind(this);
         this.getMatch = this.getMatch.bind(this);
-        this.state = {
-            matchid: 1
-        };
     }
 
-    componentDidMount() {
-        const userId = this.props.match.params.userId;
-    }
+    componentDidMount() {}
 
     // Get match by GET /api/matchUser/<uid>
-    getMatch = function(userId) {
+    getMatch(userId) {
         if (userId !== null) {
             fetch(SCHEDULER_URL + "/matchUser/" + userId, {method: 'GET'})
                 .then(response => {
@@ -31,20 +24,17 @@ class Matches extends Component {
                         return response.json();
                     }
                     })
-                .then(response =>
-                    // console.log(response)
-                    // this.setState({matchid: response})
-                    this.props.history.push(`/users/` + response)
+                .then(response => {
+                        if (response > 0) {
+                            this.props.history.push(`/users/` + response);
+                        } else {
+                            window.alert('Cannot find any user who has common available time slots!');
+                        }
+                    }
                 )
                 .catch(() => this.props.history.push("/"));
         }
     }
-
-  //   routeChange() {
-  //       let path = `/users/` + this.state.matchid;
-  //       // let path = `/users/` + this.props.match.params.userId;
-  //       this.props.history.push(path);
-  // }
 
     render() {
         return(
