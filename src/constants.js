b2ef5938e30b092823c68b381e765442335b3229
Sheng Yu <syu126@ucsc.edu>
2021-12-11 21:32:30 -0500

    updated constant urls

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,11 @@
-export const COMPOSITION_URL = "http://localhost:5000/api";
-export const USR_ADDR_URL = "http://localhost:5001/api";
-export const USR_PREF_URL = "http://localhost:5002/api";
-export const SCHEDULER_URL = "http://localhost:5003/api";
-export const USR_REVIEW_URL = "http://localhost:5004/api";
+// export const COMPOSITION_URL = "http://localhost:5000/api";
+// export const USR_ADDR_URL = "http://localhost:5001/api";
+// export const USR_PREF_URL = "http://localhost:5002/api";
+// export const SCHEDULER_URL = "http://localhost:5003/api";
+// export const USR_REVIEW_URL = "http://localhost:5004/api";
+
+export const COMPOSITION_URL = "https://defw6e7xqt3no.cloudfront.net";
+export const USR_ADDR_URL = "https://defw6e7xqt3no.cloudfront.net";
+export const USR_PREF_URL = "https://defw6e7xqt3no.cloudfront.net";
+export const SCHEDULER_URL = "https://defw6e7xqt3no.cloudfront.net";
+export const USR_REVIEW_URL = "https://defw6e7xqt3no.cloudfront.net";
